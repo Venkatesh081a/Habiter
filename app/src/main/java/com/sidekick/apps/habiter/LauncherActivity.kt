7a7b4fd72c55c3369de690911f44d5e40475cf23
harshm9497 <lostinmyworld9497@gmail.com>
2018-08-24 23:22:00 +0530

    design changes on launcher activity

--- app/src/main/java/com/sidekick/apps/habiter/LauncherActivity.kt
+++ app/src/main/java/com/sidekick/apps/habiter/LauncherActivity.kt
@@ -7,17 +7,21 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.preference.PreferenceManager
+import android.support.v4.app.FragmentManager
 import android.transition.Fade
 import android.transition.Slide
 import android.transition.Transition
 import android.transition.TransitionManager
+import android.util.Log
 import android.view.Gravity
 import android.view.Gravity.BOTTOM
 import android.view.Gravity.TOP
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_launcher.*
 
 class LauncherActivity : AppCompatActivity() {
     private lateinit var layoutContainer: ViewGroup
@@ -26,6 +30,9 @@ class LauncherActivity : AppCompatActivity() {
     private lateinit var animationDrawable: AnimationDrawable
     private lateinit var slideFromTop:Transition
     private lateinit var slideFromBottom:Transition
+    private lateinit var submitButton:Button
+    private lateinit var nameEditText:EditText
+    private lateinit var nameTextView:TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +45,9 @@ class LauncherActivity : AppCompatActivity() {
 
     private fun initializeWidgets() {
         layoutContainer = findViewById(R.id.launcher_container)
+        submitButton = layoutContainer.findViewById(R.id.user_name_submit_button)
+        nameEditText = layoutContainer.findViewById(R.id.user_name_edit_text)
+        nameTextView = layoutContainer.findViewById(R.id.user_name_text_view)
         textViewOne = layoutContainer.findViewById(R.id.launcher_text_view_one)
         startButton = layoutContainer.findViewById(R.id.launcher_text_view_two)
         startButton.setOnClickListener(startButtonOnClickListener())
@@ -51,7 +61,15 @@ class LauncherActivity : AppCompatActivity() {
     }
 
     private fun startButtonOnClickListener(): View.OnClickListener? = View.OnClickListener{
-        TODO("NEED TO ADD NAME FRAGMENT")
+            Thread().run {
+                TransitionManager.beginDelayedTransition(layoutContainer,slideFromBottom)
+                textViewOne.visibility = View.GONE
+                startButton.visibility = View.GONE
+                submitButton.visibility = View.VISIBLE
+                nameEditText.visibility = View.VISIBLE
+                nameTextView.visibility = View.VISIBLE
+
+            }
     }
 
     private fun transitionAnimation()
