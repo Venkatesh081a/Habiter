ce40e1b72b59a5c56308f9e17fa590d5b5d83b21
harshm9497 <lostinmyworld9497@gmail.com>
2018-10-06 21:56:24 +0530

    added default Toolbar for every fragment that uses this class

--- app/src/main/java/com/sidekick/apps/habiter/SingleFragmentActivity.kt
+++ app/src/main/java/com/sidekick/apps/habiter/SingleFragmentActivity.kt
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toolbar
 
 /**
  * Created by HaRRy on 8/22/2018.
@@ -13,13 +14,20 @@ import android.support.v7.app.AppCompatActivity;
 public abstract class SingleFragmentActivity:AppCompatActivity()
 {
     lateinit var fragmentManager:FragmentManager
+    lateinit var toolbar:android.support.v7.widget.Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_single_fragment)
+        setUpToolbar()
         fragmentManager = supportFragmentManager
         val fragment:Fragment = addFragment()
         fragmentManager.beginTransaction().add(R.id.single_fragment_activity_container,fragment).commit()
 
+    }
+    private fun setUpToolbar() {
+        toolbar = findViewById(R.id.single_activity_toolbar)
+        setSupportActionBar(toolbar)
+        window.statusBarColor = resources.getColor(R.color.colorPrimary)
     }
     abstract fun addFragment():Fragment
 
